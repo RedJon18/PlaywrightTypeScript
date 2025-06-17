bdd9652d8de69f6774170e5fcce06e1ccbc55b27
Jon Yore <jonnyyore@gmail.com>
2025-06-17 16:14:11 -0700

    Updated locators

--- tests/metaTest.spec.ts
+++ tests/metaTest.spec.ts
@@ -3,7 +3,9 @@ import { expect, test } from "@playwright/test";
 test.describe("This is a sample test", () => {
   test("Test the homepage", async ({ page }) => {
     await page.goto("https://www.meta.com/ai-glasses/");
-    await expect(page).toHaveTitle("Ray-Ban Meta AI Glasses | Meta Store");
+    await expect(page).toHaveTitle(
+      "Meta AI Glasses | Ray-Ban Meta | Meta Store"
+    );
     await page.getByRole("link", { name: "Shop all styles" }).click();
     await expect(page).toHaveURL(/shop-all/);
   });
