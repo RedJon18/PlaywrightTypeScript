554634eab74c4195aaf9eefe43dca9092aae7c13
Jon Yore <jonnyyore@gmail.com>
2025-06-17 16:57:26 -0700

    Adding visibility check to help with page load before checking title

--- tests/metaTest.spec.ts
+++ tests/metaTest.spec.ts
@@ -3,6 +3,9 @@ import { expect, test } from "@playwright/test";
 test.describe("This is a sample test", () => {
   test("Test the homepage", async ({ page }) => {
     await page.goto("https://www.meta.com/ai-glasses/");
+    await expect(
+      page.getByRole("link", { name: "Shop all styles" })
+    ).toBeVisible();
     await expect(page).toHaveTitle(
       "Meta AI Glasses | Ray-Ban Meta | Meta Store"
     );
