6377359bc37647773665e5c841440f51c4be0a35
Jon Yore <jonnyyore@gmail.com>
2025-06-17 17:13:48 -0700

    Trying a different path

--- tests/metaTest.spec.ts
+++ tests/metaTest.spec.ts
@@ -2,14 +2,11 @@ import { expect, test } from "@playwright/test";
 
 test.describe("This is a sample test", () => {
   test("Test the homepage", async ({ page }) => {
-    await page.goto("https://www.meta.com/ai-glasses/");
-    await expect(
-      page.getByRole("link", { name: "Shop all styles" })
-    ).toBeVisible();
+    await page.goto("https://www.meta.com");
     await expect(page).toHaveTitle(
-      "Meta AI Glasses | Ray-Ban Meta | Meta Store"
+      "Meta - Shop MR, VR Headsets & AI Glasses | Meta Store"
     );
-    await page.getByRole("link", { name: "Shop all styles" }).click();
+    await page.getByRole("link", { name: "Shop AI glasses" }).click();
     await expect(page).toHaveURL(/shop-all/);
   });
 });
